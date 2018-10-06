e9ec2b8fbe6269229c10f4bd7eec21d8aa1f3b7f
victoria <victoria.kildyushevskaya@gmail.com>
2018-10-07 01:18:54 +0200

    Added Bokeh

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 from bokeh.plotting import figure
 from bokeh.embed import components
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 
 app = Flask(__name__)
 
@@ -35,17 +37,29 @@ def index():
 def result():
    if request.method == 'POST':
       result = request.form
-      startdate = result["start"]
-      enddate = result["end"]
-      mydata = Quandl.get("ZILLOW/C25709_ZRISFRR", authtoken="QT-coVZNkYPJCs6R9Tkj", startdate=startdate, enddate=enddate)
-      plot = figure(plot_height=300, sizing_mode='scale_width')
+      # startdate = result["start"]
+      # enddate = result["end"]
+      # mydata = Quandl.get("ZILLOW/C25709_ZRISFRR", authtoken="QT-coVZNkYPJCs6R9Tkj", startdate=startdate, enddate=enddate)
 
-      x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-      y = [2 ** v for v in x]
 
-      plot.line(x, y, line_width=4)
+      # init a basic bar chart:
+      # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+      fig = figure(plot_width=600, plot_height=600)
+      fig.vbar(
+          x=[1, 2, 3, 4],
+          width=0.5,
+          bottom=0,
+          top=[1.7, 2.2, 4.6, 3.9],
+          color='navy'
+      )
 
-      script, div = components(plot)
+      # grab the static resources
+      js_resources = INLINE.render_js()
+      css_resources = INLINE.render_css()
+
+      # render template
+      script, div = components(fig)
+
       # INSERT CODE HERE WHICH PLOTS A DATA
 
 
@@ -53,7 +67,10 @@ def result():
 
       # END OF PLOTTING DATA
 
-      return render_template("result.html", result = result, script=script, div=div)
+      return render_template("result.html", result = result, plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,)
 
 
 @app.route('/about')

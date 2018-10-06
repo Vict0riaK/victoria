985c2d0e5de75dc4847c601bd2407607bb9c4c23
victoria <victoria.kildyushevskaya@gmail.com>
2018-10-06 23:23:32 +0200

    Added First Page

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ import Quandl
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
+    name = TextField('Name:',)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     form = ReusableForm(request.form)
     print (form.errors)
     if request.method == 'POST':
-        name = request.form['name']
-        print (name)
+        startdate = request.form['Start Date']
+        # enddate = request.form['End Date']
+
+        print (startdate)
 
         if form.validate():
             # Save the comment here.
-            flash('Hello ' + name)
+            flash('Hello ' + startdate)
         else:
             flash('All the form fields are required. ')
 

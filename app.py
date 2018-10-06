8c94a3920ba8658e80be4234e0db66cfa23a8c44
victoria <victoria.kildyushevskaya@gmail.com>
2018-10-06 21:17:41 +0200

    ADDED QUANDL DATA

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect
+import quandl
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-
+  # return render_template('index.html')
+    print mydata
 @app.route('/about')
 def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
+  mydata = quandl.get("ZILLOW/C25709_ZRISFRR", authtoken="QT-coVZNkYPJCs6R9Tkj")
+
